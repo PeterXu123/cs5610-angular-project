f4b096e85348f83991cb7fdb7d794a586aef4d81
peterx <xu.kang@husky.neu.edu>
2020-11-16 20:07:20 -0500

    fixed bug on quiz service url

--- src/app/services/quiz.service.client.ts
+++ src/app/services/quiz.service.client.ts
@@ -2,9 +2,9 @@ import {Injectable} from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class QuizzesServiceClient {
   findAllQuizzes = () =>
-    fetch('http://localhost:3000/api/quizzes')
+    fetch('https://cs5610-2020-fall-hw8-server.herokuapp.com/api/quizzes')
       .then(response => response.json())
   findQuizById = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    fetch(`https://cs5610-2020-fall-hw8-server.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 }
